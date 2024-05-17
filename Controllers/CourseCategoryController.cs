af070ba854d0cad6943efefab9b22746499b8a90
Oktysutc <126988680+Oktysutc@users.noreply.github.com>
2024-05-17 20:53:58 +0300

    addmodelcontroller

---
+++ Controllers/CourseCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using EducationManagementPlatform.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationManagementPlatform.Controllers
+{
+    public class CourseCategoryController : Controller
+    {    /// <summary>
+    /// ////////////
+    /// </summary>
+        private readonly ApplicationDbContext _applicationDbContext;
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Utility/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EducationManagementPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationManagementPlatform.Utility
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+       
+        public DbSet<CourseCategory>CourseCategories { get; set; }
+    }
+}
